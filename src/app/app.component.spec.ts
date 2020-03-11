db6cad2cae99fd94805cc329d8473b97129ea674
James Barrett <james.barrett@santander.co.uk>
2020-03-11 16:40:18 +0000

    App component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RegionCountriesSelectorComponent } from './components/region-countries-selector/region-countries-selector.component';
-import { CountryDetailsComponent } from './components/country-details/country-details.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Country } from './models/Country';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let countryData: Country;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
@@ -15,12 +19,21 @@ describe('AppComponent', () => {
       imports: [
         HttpClientModule
       ]
-    }).compileComponents();
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.debugElement.componentInstance;
+      countryData = {name: 'test', capital: 'test', population: 9999, currencies: ['test, test'], flag: 'test'};
+    });
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+  it('should set the countryDataToView variable so it can be passed to country details ', () => {
+    component.viewCountryData(countryData);
+    fixture.detectChanges();
+    expect(component.countryDataToView).toBe(countryData);
+  });
+
 });
